8f97087421938b550a54e308d830ad7bd5d36b5e
Kay <kayreambonanza@gmail.com>
2026-04-19 16:19:34 +0800

    integrate and intialise tailwind css; add detect button; add main container

--- web_app.py
+++ web_app.py
@@ -1,14 +1,34 @@
 import streamlit as st
 import pandas as pd
+import st_tailwind as tw
+from st_tailwind import tw_wrap
+
+# Call to integrate Tailwind
+tw.initialize_tailwind()
 
 st.write("[Best model name]-based EggCrack Detection")
 
+# Upload Logic
 
-with st.container(border=True, width="stretch", height=400):
+audio_container = tw_wrap(st.container)(
+    classes="border-1 border-black"
+)
+with st.container(border=True, width="stretch", height=300):
     audio_file = st.file_uploader("Upload audio file", type=["mp3","wav","ogg"])
 
     if audio_file is not None:
         st.audio(audio_file, format=f'audio/{audio_file.type.split("/")[-1]}')
+
+# Detect Button
+detect_button = tw_wrap(st.button)(
+    "Detect Cracks", 
+    key="detect_action",
+    classes="w-fit bg-green-600 text-white rounded-full border-1 border-black justify-center-safe place-items-center"
+    )
+
+if detect_button:
+    st.info("Analysing audio for cracks...") # Call CNN model here for later
+
 
 # df = pd.read_csv("data.csv")
 # st.line_chart(df)
